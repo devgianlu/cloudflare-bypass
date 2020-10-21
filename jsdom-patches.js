78f6d0eede661db1da2662fce4e6b22582e48fb5
Gianlu <altomanigianluca@gmail.com>
2020-10-21 21:42:14 +0200

    Patched 6feeed9d

--- jsdom-patches.js
+++ jsdom-patches.js
@@ -1,5 +1,6 @@
 const imgProbe = require('probe-image-size')
 const crypto = require('crypto')
+const log = require('./logging')
 
 function patchSha256(ctx) {
 	ctx.window['SHA256'] = function (input) {
@@ -44,8 +45,61 @@ function patch89d70e43(ctx) {
 	}
 }
 
+function patch6feeed9d(ctx) {
+	const LOOKUP = [{h: 24, i: 2}, {h: 13, i: 4}]
+
+	function shouldHandle(child) {
+		return child.style.fontSize === '10px' && child.style.border === '1px solid blue' && child.innerHTML.indexOf('width:2px') !== -1
+	}
+
+	const _getElementById = ctx.document.getElementById.bind(ctx.document)
+	ctx.document.getElementById = function (id) {
+		const elm = _getElementById(id)
+		if (id !== 'challenge-form')
+			return elm
+
+		const _appendChild = elm.appendChild.bind(elm)
+		elm.appendChild = function (child) {
+			const result = _appendChild(child)
+			if (child.tagName !== 'SPAN' || !shouldHandle(child))
+				return result;
+
+			(function () {
+				let width = child.style.width
+				let offsetHeight = child.offsetHeight
+
+				Object.defineProperty(child.style, 'width', {
+					get: function () {
+						return width
+					},
+					set: function (value) {
+						width = value
+
+						for (let i = 0; i < LOOKUP.length; i++) {
+							const entry = LOOKUP[i]
+							if (entry.i === parseInt(value.substr(0, value.length - 2)))
+								offsetHeight = entry.h
+						}
+					}
+				})
+
+				Object.defineProperty(child, 'offsetHeight', {
+					get: function () {
+						return offsetHeight
+					}
+				})
+			})()
+
+			return result
+		}
+
+		return elm
+	}
+}
+
 module.exports = function (jsdom) {
 	const ctx = jsdom.getInternalVMContext()
 	patchSha256(ctx)
 	patch89d70e43(ctx)
+	patch6feeed9d(ctx)
 }
