430a254402b29740a46035f5f1920e151f943a9b
Gianlu <altomanigianluca@gmail.com>
2020-10-24 12:12:51 +0200

    Patched c48c9b6c again

--- script-patches.js
+++ script-patches.js
@@ -1,6 +1,7 @@
 function patch_c48c9b6c(script) {
 	script = script.replace(/\(\+\(_0x.\.innerHTML == _0x.\.innerHTML && _0x.\.innerText != _0x.\.innerText\)\)/, '1')
 	script = script.replace(/\(\+\(!!_0x.\.maxLength && \(_0x.\.maxLength === _0x.\.maxLength\)\)\)/, '1')
+	script = script.replace(/\(\+\(\(!!_0x.\.getElementById\(_\[.+?]\)\) && \(_0x.\.getElementById\(_\[.+?]\)\.value === _\[.+?]\)\)\)/, '1')
 	return script
 }
 
